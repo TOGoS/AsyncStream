dc7a552bdba7bffee0c9e7e9b6192084d34292ce
Dan Stevens <stevens@earthit.com>
2013-11-04 14:51:49 -0600

    Declare E type parameter of StreamSource to extend Throwable.

--- src/togos/asyncstream/StreamSource.java
+++ src/togos/asyncstream/StreamSource.java
@@ -1,6 +1,6 @@
 package togos.asyncstream;
 
-public interface StreamSource<T, E>
+public interface StreamSource<T, E extends Throwable>
 {
 	public void pipe( StreamDestination<? super T, ? extends E> dest );
 }
